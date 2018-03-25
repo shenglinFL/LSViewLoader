6330ad72e5f015b4d6fbc830391c8888f1ec900c
林盛 <linsheng@xrjiot.com>
2018-03-25 14:06:20 +0800

    Init

---
+++ LSViewLoader/LSViewLoader/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LSViewLoader
+//
+//  Created by 林盛 on 2018/3/25.
+//  Copyright © 2018年 linsheng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
